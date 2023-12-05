8b85493300902b94acb3f3a72099cf0a2b107186
rooban33 <shajithgunasekaran@gmail.com>
2023-12-05 21:59:39 +0530

    1st

--- flask/app.py
+++ flask/app.py
@@ -94,30 +94,55 @@ def send_image():
     if file.filename == '':
         return jsonify({'error': 'No selected file'})
 
-    image_bytes = file.read()
     target_ip = request.headers.get('Target-IP')
 
     # Create a socket to send the image
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((target_ip, 12345))  # Port number for communication (change as needed)
 
-    # Send the image
-    s.sendall(image_bytes)
+    # Read and send the image in chunks
+    while True:
+        image_chunk = file.read(4096)  # Read 4KB chunks of the image file
+        if not image_chunk:
+            break
+        s.sendall(image_chunk)
+
     s.close()
 
     return jsonify({'message': 'Image sent successfully'})
 
 @app.route('/receive-image', methods=['POST'])
 def receive_image():
-    target_ip = request.remote_addr
-    received_image_bytes = request.data
+    host = '0.0.0.0'  # Use '0.0.0.0' to listen on all available interfaces
+    port = 12345  # Use the same port number used by the server
+
+    # Create a socket to listen for incoming connections
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(1)  # Listen for one incoming connection
+
+    print(f"Waiting for connection on port {port}...")
+    client_socket, client_address = server_socket.accept()
+    print(f"Connection established with {client_address}")
+
+    # Receive and save the image
+    received_image = b''  # Initialize an empty byte string to store the image data
+    while True:
+        image_chunk = client_socket.recv(4096)  # Receive 4KB chunks of the image data
+        if not image_chunk:
+            break
+        received_image += image_chunk
 
     # Save the received image to a file
-    received_image_path = f'received_images/received_image_from_{target_ip}.jpg'
-    with open(received_image_path, 'wb') as file:
-        file.write(received_image_bytes)
+    with open('received_image.jpg', 'wb') as file:
+        file.write(received_image)
 
-    return jsonify({'message': 'Image received successfully'})
+    print("Image received and saved as 'received_image.jpg'")
+
+    # Close the sockets
+    client_socket.close()
+    server_socket.close()
+
 
 @app.route('/get-received-image')
 def get_received_image():
